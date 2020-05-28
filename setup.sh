d799302e002ffad3ad3af514d45d39244a27a559
Florian Hoffmann <s5870980@tu-dresden.de>
2020-05-28 17:23:03 +0200

    adding nginx and neo4j, gds-plugin installed for neo4j

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Linux Version
+#docker-compose -f /home/floo/workspace/ConfluenceHelperServer/docker-compose.yml up -d neo4j
+#docker-compose -f /home/floo/workspace/WeatherServer/docker-compose.yml up -d webserver
+
+# Windows Version
+docker-compose -f D:\workspace\ConfluenceHelperServer\docker-compose.yml up -d neo4j
+docker-compose -f D:\workspace\ConfluenceHelperServer\docker-compose.yml up -d webserver
